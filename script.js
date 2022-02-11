63881af22edfe352e97277a319328c68202619df
Bayastanasnbekov <baiaskoas07@gmail.com>
2022-02-11 15:34:41 +0600

    commit

---
+++ script.js
@@ -0,0 +1,65 @@
+// let example1 = document.querySelector('#example1');
+// example1.addEventListener('click', function(event){
+  
+//   window.location.href = "http://bing.com"; // window. без него тоже будет работать
+//   event.preventDefault(); //кнопка перестанет работать
+  
+//   //alert ('Hello world');
+//  });
+// document.body.addEventListener('contextmenu', function(event){
+//   event.preventDefault();
+// });
+
+// document.body.addEventListener('dbclick', function(event){
+//   event.preventDefault();
+// });
+
+// document.querySelector('div').addEventListener('dbclick', function(event){
+//   console.log(event);
+//   event.preventDefault();
+// });
+
+// document.querySelector('div').addEventListener('dbclick', function(event){
+//   alert('Hello!');
+//   //event.preventDefault(); --doesn't work
+// });
+
+// document.querySelector('div').addEventListener('mousemove', function(event){
+//   event.preventDefault();
+// });
+
+// let example = document.createElement('section', );
+// example.width = "3rm"
+// example.textContent = '👻';
+// example.style.position = 'absolute';
+// document.body.append(example);
+
+// document.body.addEventListener('mousemove', function(event){
+//   example.style.top = event.clientY + 'px';
+//   example.style.left = event.clientX + 'px';
+//   console.log(event.clientX);
+//   console.log(event.clientY);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
